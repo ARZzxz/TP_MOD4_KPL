fa517d0f44e1562c057ac723c26dfa1ae9e51360
ARZzxz <achmadrizky0211@gmail.com>
2022-03-18 17:33:33 +0700

    "mengubah master"

--- TP4_KPL/Program.cs
+++ TP4_KPL/Program.cs
@@ -10,30 +10,6 @@ namespace TP4_KPL
     {
         static void Main(string[] args)
         {
-            HaloGeneric<string> halo = new HaloGeneric<string>();
-            String x = " Achmad Rizky";
-            halo.SapaUser(x);
-            DataGeneric<String> data = new DataGeneric<string>("1302200027");
-            data.PrintData();
-        }
-    }
-    public class HaloGeneric<T>
-    {
-        public void SapaUser(T x)
-        {
-            Console.WriteLine("Halo" + x);
-        }
-    }
-    public class DataGeneric<T>
-    {
-        private T data;
-        public DataGeneric(T data)
-        {
-            this.data = data;
-        }
-        public void PrintData()
-        {
-            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
         }
     }
 }
